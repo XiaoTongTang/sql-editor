07d53e3a3f620e992660af5f755e675609c6cb35
TextTang <Text.Tang@Amway.com>
2025-12-20 12:08:19 +0800

    配置好了vscodeExtension的开发环境，感谢vite-plugin-vscode

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import vscode from '@tomjs/vite-plugin-vscode';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag: string) => tag.startsWith('vscode-'),
+        },
+      },
+    }),
     vueJsx(),
     vueDevTools(),
+    vscode({
+      extension: {
+        minify: false,
+      },
+    }),
   ],
   resolve: {
     alias: {
